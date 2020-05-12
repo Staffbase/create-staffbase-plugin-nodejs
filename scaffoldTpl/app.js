c6dc40e8294ed2e627e5bdc36c3994f3a6b4880f
Daniel Große <daniel.grosse@staffbase.com>
2020-05-12 13:01:32 +0200

    Moves from jade to pug

--- scaffoldTpl/app.js
+++ scaffoldTpl/app.js
@@ -18,7 +18,7 @@ ssoMiddleWare = require('@staffbase/staffbase-plugin-sdk').middleware(staffbaseK
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
