c5bae486b83a5d372fb47c672c4c352a88a70bc4
Max Scholz <max@staffbase.com>
2025-02-19 09:02:18 +0100

    chore: update formatting

--- scaffoldTpl/app.js
+++ scaffoldTpl/app.js
@@ -1,77 +1,79 @@
-let express = require('express');
-let path = require('path');
-let favicon = require('serve-favicon');
-let logger = require('morgan');
-let cookieParser = require('cookie-parser');
-let bodyParser = require('body-parser');
+let express = require("express");
+let path = require("path");
+let favicon = require("serve-favicon");
+let logger = require("morgan");
+let cookieParser = require("cookie-parser");
+let bodyParser = require("body-parser");
 
-let index = require('./routes/index');
-let users = require('./routes/users');
+let index = require("./routes/index");
+let users = require("./routes/users");
 
 let app = express();
 
 const staffbaseKey = null;
 const pluginID = null;
 let ssoMiddleWare;
-ssoMiddleWare = require('@staffbase/staffbase-plugin-sdk').middleware(staffbaseKey, pluginID);
-
+ssoMiddleWare = require("@staffbase/staffbase-plugin-sdk").middleware(
+  staffbaseKey,
+  pluginID
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', index);
-app.use('/users', users);
+app.use("/", index);
+app.use("/users", users);
 
 // Frontend Handler
-app.use('/frontEnd', ssoMiddleWare);
-app.get('/frontEnd', function(req, res) {
-  res.render('plugin', req.sbSSO);
+app.use("/frontEnd", ssoMiddleWare);
+app.get("/frontEnd", function (req, res) {
+  res.render("plugin", req.sbSSO);
 });
 
 // Setup SSO Milleware on the endpoint server
-app.use('/staffbase/sso/backoffice', ssoMiddleWare);
+app.use("/staffbase/sso/backoffice", ssoMiddleWare);
 
 // Handle SSO response from server with decoded data
-app.get('/staffbase/sso/backoffice', function(req, res) {
+app.get("/staffbase/sso/backoffice", function (req, res) {
   // Middleware was able to decode the token
   if (req.sbSSO) {
-    console.log('Decoded data on backend(admin):', req.sbSSO);
-    res.render('plugin', req.sbSSO);
+    console.log("Decoded data on backend(admin):", req.sbSSO);
+    res.render("plugin", req.sbSSO);
     return res.end();
   }
   res.json({
     error: {
-      msg: 'Unable to get token information.',
+      msg: "Unable to get token information.",
     },
   });
   return res.end();
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  let err = new Error('Not Found');
+app.use(function (req, res, next) {
+  let err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
